b98a372ef3a3f8bee50dd5b562ae1553beaa233d
xiaole1204 <zxc25773313@gmail.com>
2019-10-30 14:18:57 +0800

    0.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
-public class Main {
+//import java.util.Scanner;
+////99乘法表
+//public class Main {
+//    public static void main(String[] args) {
+//	Scanner scn = new Scanner(System.in);
+//	int n = scn.nextInt();
+//	int m = scn.nextInt();
+//	for (int i= 1 ;i<=n;i++) {
+//	    for (int j = 1; j<=m;j++){
+//            System.out.print((i * j)+"\t");
+//        }
+//        System.out.println();
+//        }
+//    }
+//}
 
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
