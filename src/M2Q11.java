1df31b878dfff393b137653e32d4dd62222cf39f
xiaole1204 <zxc25773313@gmail.com>
2019-10-31 19:39:30 +0800

    0.0

---
+++ src/M2Q11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class M2Q11 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for (int i = 1;i<=n;i++){
+            if (n%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/M2Q25.java
@@ -0,0 +1,5 @@
+public class M2Q25 {
+    public static void main(String[] args) {
+
+    }
+}
