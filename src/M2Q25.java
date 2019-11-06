3a40856654d9d6af8552df84d6aaf259f8aea4cd
xiaole1204 <zxc25773313@gmail.com>
2019-11-06 13:48:43 +0800

    0.0

--- src/M2Q25.java
+++ src/M2Q25.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class M2Q25 {
     public static void main(String[] args) {
-
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int sum = 0;
+        for (int i = 2;i<=n;i++){
+            int j = i;
+            while (j % 2 ==0){
+                j /=2;
+            } while (j%3==0){
+                j/=3;
+            } while (j%5==0){
+                j/=5;
+            }
+            if (j == 1) {
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
     }
 }
